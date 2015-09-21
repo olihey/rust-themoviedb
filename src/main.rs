a60333007485d8b2aeeea09cf2c3e7c774be9368
Oliver Heyme <olihey@googlemail.com>
2015-09-21 22:25:04 +0200

    More save handling

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,18 @@ struct TheMovieDB {
 	api_key: &'static str,
 	base_url: &'static str,
 	http_client: Client,
-
 }
 
 impl TheMovieDB {
-	fn new(themoviedb_api_key: &'static str) -> TheMovieDB {
+	fn new(themoviedb_api_key: &'static str) -> Result<TheMovieDB, String> {
 		let new_instance = TheMovieDB{api_key: themoviedb_api_key,
 			base_url: "https://api.themoviedb.org/3",
 			http_client: Client::new()};
 
-		new_instance
+		let configuration_data = new_instance.get_json_data_for("/configuration");
+		println!("{:?}", configuration_data);
+
+		Ok(new_instance)
 	}
 
 	fn get_url_for(& self, url_string: &'static str) -> String {
@@ -33,24 +35,28 @@ impl TheMovieDB {
 	fn get_json_data_for(& self, url_string: &'static str) -> Result<Json, String> {
 		let call_url = self.get_url_for(url_string);
 
-		let mut res = match self.http_client.get(&call_url).send() {
+		let mut response = match self.http_client.get(&call_url).send() {
 			Ok(r) => r,
 			Err(_) => {
 				return Err("Error during HTTPS request".to_string());
 			}
  		};
-		assert_eq!(res.status, hyper::Ok);
+
+		if hyper::Ok != response.status {
+			return Err("No HTTPS Status 200 (OK)".to_string());
+		}
 
 		let mut result = String::new();
-		res.read_to_string(& mut result);
+		if response.read_to_string(& mut result).is_err() {
+			return Err("Error reading response".to_string());
+		}
 
-		let json_data = Json::from_str(&result).unwrap();
-		let json_obj = json_data.as_object().unwrap();
+		let json_data = match Json::from_str(&result) {
+			Ok(d) => d,
+			Err(_) => return Err("Error while readin JSON data".to_string())
+		};
 
-		println!("{:?}", json_obj);
-
-//		Some(json_data)
-		Err("FAIL".to_string())
+		Ok(json_data)
 	}
 
 	fn test(& self) -> String {
@@ -78,5 +84,5 @@ fn it_works() {
 fn main() {
     let themdb = TheMovieDB::new("6da14fa0b6231874a56ee667a505cdcc");
 
-    println!("{}", themdb.test());
+    println!("{}", themdb.unwrap().test());
 }
